303e71a4daf181bdfa67e09c69002b763d7db757
Max O'Meara <mxomeara@bu.edu>
2024-12-10 21:15:34 -0500

    update test_book

--- tests/test_book_model.py
+++ tests/test_book_model.py
@@ -1,6 +1,7 @@
 import pytest
-from Library.models.book_model import Book
+from models.book_model import *
 
+@pytest.fixture
 def test_book_initialization():
     book = Book(id=1, title="Animal Farm", author="George Orwell", isbn="1234567890", status="Read")
     assert book.id == 1
@@ -8,8 +9,10 @@ def test_book_initialization():
     assert book.author == "George Orwell"
     assert book.isbn == "1234567890"
     assert book.status == "Read"
+    return book
 
 
+@pytest.fixture
 def test_book_to_dict():
     book = Book(id=3, title="Fahrenheit 451", author="Ray Bradbury", isbn="0987654321", status="Reading")
     book_dict = book.to_dict()
@@ -21,9 +24,11 @@ def test_book_to_dict():
         "status": "Reading"
     }
 
+@pytest.fixture
 def test_book_str():
     book = Book(id=4, title="The Great Gatsby", author="F. Scott Fitzgerald", status="Read")
     assert str(book) == "'The Great Gatsby' by F. Scott Fitzgerald (Read)"
+    return book
 
 if __name__ == "__main__":
     pytest.main()
